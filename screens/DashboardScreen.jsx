428a980a293c80945f7a5e573159aebdb2d38fff
Chan Wai Hsuen <chanwaihsuen@gmail.com>
2020-12-20 18:06:23 +0800

    chore(font): adding custom fonts

--- screens/DashboardScreen.jsx
+++ screens/DashboardScreen.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
 import { Switch, Subheading, Button } from 'react-native-paper'
 import { StyleSheet, Text, View } from 'react-native'
 import SafeAreaView from '../features/base/SafeAreaView'
@@ -12,33 +13,41 @@ export default function DashboardScreen() {
   const [isSwitchTxtOn, setIsSwitchTxtOn] = React.useState(false)
   const [isSwitchImgOn, setIsSwitchImgOn] = React.useState(false)
 
-  return (
-    <SafeAreaView>
-      <ScreenHeading>Settings For Cards</ScreenHeading>
-      <View style={[styles.body]}>
-        <View style={[styles.content]}>
-          <View>
-            <LogoSvg />
-          </View>
-          <View style={[styles.switch]}>
-            <Subheading>
-              Show Text <Switch value={isSwitchTxtOn} onValueChange={() => setIsSwitchTxtOn(!isSwitchTxtOn)} />
-            </Subheading>
-          </View>
-          <View style={[styles.switch]}>
-            <Subheading>
-              Show Images <Switch value={isSwitchImgOn} onValueChange={() => setIsSwitchImgOn(!isSwitchImgOn)} />
-            </Subheading>
-          </View>
-          <View style={[styles.switch]}>
-            <Button icon="cards" mode="contained" onPress={() => navigation.navigate('Cards')}>
-              Generate Cards
-            </Button>
+  let [fontsLoaded] = useFonts({
+    'Campton-Medium': require('../assets/fonts/Campton-Medium.otf'),
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <SafeAreaView>
+        <ScreenHeading>Settings For Cards</ScreenHeading>
+        <View style={[styles.body]}>
+          <View style={[styles.content]}>
+            <View>
+              <LogoSvg />
+            </View>
+            <View style={[styles.switch]}>
+              <Subheading>
+                Show Text <Switch value={isSwitchTxtOn} onValueChange={() => setIsSwitchTxtOn(!isSwitchTxtOn)} />
+              </Subheading>
+            </View>
+            <View style={[styles.switch]}>
+              <Subheading>
+                Show Images <Switch value={isSwitchImgOn} onValueChange={() => setIsSwitchImgOn(!isSwitchImgOn)} />
+              </Subheading>
+            </View>
+            <View style={[styles.switch]}>
+              <Button icon="cards" mode="contained" onPress={() => navigation.navigate('Cards')}>
+                Generate Cards
+              </Button>
+            </View>
           </View>
         </View>
-      </View>
-    </SafeAreaView>
-  )
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
